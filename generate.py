d77646b4892658e802c3dbe15d535bb644d8dfb7
michalhabera <michal.habera@gmail.com>
2017-08-18 18:22:08 +0200

    Encoding and savedir added

--- generate.py
+++ generate.py
@@ -5,31 +5,40 @@ Script to generate a range of XDMF files with Finite Element "Index + Data" form
 
 from dolfin import *
 
+encoding = XDMFFile.Encoding_HDF5
+savedir = "generated_files"
 
 def output_scalar_xdmf(mesh, scalar_expression, file_suffix, elem_family, elem_degree):
     Q = FunctionSpace(mesh, elem_family, elem_degree)
     F = Function(Q)
     F.interpolate(Expression(scalar_expression, degree=1))
-    xdmf = XDMFFile("scalar_" + elem_family + str(elem_degree) + "_" + file_suffix + ".xdmf")
-    xdmf.write_checkpoint(F, "f", 0, XDMFFile.Encoding_ASCII)
+    xdmf = XDMFFile(savedir + "/scalar_" + elem_family + str(elem_degree) + "_" + file_suffix + ".xdmf")
+    xdmf.write_checkpoint(F, "f", 0, encoding)
 
 
 def output_vector_xdmf(mesh, vector_expression, file_suffix, elem_family, elem_degree):
     Q = VectorFunctionSpace(mesh, elem_family, elem_degree)
     F = Function(Q)
     F.interpolate(Expression(vector_expression, degree=1))
-    xdmf = XDMFFile("vector_" + elem_family + str(elem_degree) + "_" + file_suffix + ".xdmf")
-    xdmf.write_checkpoint(F, "f", 0, XDMFFile.Encoding_ASCII)
+    xdmf = XDMFFile(savedir + "/vector_" + elem_family + str(elem_degree) + "_" + file_suffix + ".xdmf")
+    xdmf.write_checkpoint(F, "f", 0, encoding)
 
 
 # 2D
 mesh = UnitSquareMesh(10, 10)
 for elem_family in ["CG", "DG"]:
-    for elem_degree in [1, 2]:
+    elem_degrees = [1, 2]
+    if elem_family == "DG":
+        elem_degrees.append(0)
+    for elem_degree in elem_degrees:
         output_scalar_xdmf(mesh, "sin(2*pi*x[0])*cos(2*pi*x[1])", "2D", elem_family, elem_degree)
 
+for elem_family in ["RT"]:
+    for elem_degree in [1]:
+        output_scalar_xdmf(mesh, ("sin(2*pi*x[0])", "cos(2*pi*x[1])"), "2D", elem_family, elem_degree)
+
 # 2D manifold
-mesh = UnitDiscMesh(mpi_comm_world(), 5, 1, 3)
+mesh = UnitDiscMesh.create(mpi_comm_world(), 5, 1, 3)
 mesh.init_cell_orientations(Constant((0, 0, 1)))
 for elem_family in ["CG", "DG"]:
     for elem_degree in [1, 2]:
